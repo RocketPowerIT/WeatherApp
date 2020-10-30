4912d9b0fd82907a0a14e80300b2e75e120b5044
Dmitry Kokurkin <wolfyteze@gmail.com>
2020-10-30 19:52:33 +0300

    Init()

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var appearentTemperatureLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func refreshButtonTapd(_ sender: UIButton) {
+    }
+    
 }
 
