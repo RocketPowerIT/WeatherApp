dcb3506f77b89058d29963fce9abf548c1abcf39
Dmitry Kokurkin <wolfyteze@gmail.com>
2020-10-30 19:09:35 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by wolfyteze on 30/10/2020.
+//  Copyright © 2020 wolfyteze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
